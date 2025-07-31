45cc271020aabedb54cce80d74fc63a757840160
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-07-31 20:01:54 +0200

    Refactor AddTask component to manage state and improve layout; add form elements for task input

--- src/pages/AddTask.jsx
+++ src/pages/AddTask.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { useState, useRef } from "react"
 
 const AddTask = () => {
+    const [title, setTitle] = useState('');
+    const descriptionRef = useRef();
+    const statusRef = useRef();
+
     return (
-        <div>
-            <h1>Aggiungi task</h1>
+        <div className="container mt-4">
+            <h1 className="mb-4 text-light">Aggiungi un Task</h1>
+            <div className="table-responsive">
+                <form>
+                    <div className="mb-3">
+                        <label for="exampleFormControlInput1" className="form-label">Email address</label>
+                        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                    </div>
+                    <div className="mb-3">
+                        <label for="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
