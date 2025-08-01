a409cf3f60f79b3a43b5a63c11ee443cafdc27eb
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-08-01 17:51:01 +0200

    Implement TaskDetails component to display task information, including title, description, status, and creation date; add loading state for tasks retrieval.

--- src/pages/TaskDetails.jsx
+++ src/pages/TaskDetails.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { useParams } from "react-router-dom"
+import { useGlobalContext } from "../hooks/useGlobalContext";
 
 const TaskDetails = () => {
-    return (
-        <div>
+    const { id } = useParams();
+    const { tasks } = useGlobalContext();
 
+    if (!tasks) return <div className="text-center text-light">Caricamento...</div>
+
+    const task = tasks.find(task => task.id === parseInt(id));
+    const { title, description, status, createdAt } = task;
+
+    return (
+        <div className="container">
+            <div className="row py-5">
+                <div className="col">
+                    <div className="card text-center bg-dark text-light border-secondary">
+                        <div className="card-header fw-bold border-secondary fs-3 bg-secondary">
+                            {title}
+                        </div>
+                        <div className="card-body d-flex">
+                            <p className="card-text w-50">{description}</p>
+                            <div className="w-50">
+                                <button className="btn btn-danger" onClick={() => console.log(`Elimina task ${task.id}`)}>Elimina</button>
+                            </div>
+                        </div>
+                        <div className="card-footer text-light border-secondary bg-secondary">
+                            {new Date(task.createdAt).toLocaleString('it-IT')}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
