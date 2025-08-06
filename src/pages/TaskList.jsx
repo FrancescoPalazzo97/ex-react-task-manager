629ab910edf7f430cf5a1e76e62048572a7410d7
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-08-06 13:16:32 +0200

    UPD - Milestone 11 done. Enhance TaskList component: add sorting functionality for tasks by title, status, and creation date; update UI for sortable headers with visual indicators.

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,12 +1,51 @@
 import { useGlobalContext } from "../hooks/useGlobalContext";
 import TaskRow from "../components/TaskRow";
-import { memo } from "react";
+import { memo, useState, useMemo } from "react";
 import Loader from "../components/Loader";
 
 const TaskList = memo(() => {
     const { tasks } = useGlobalContext();
+    const [sortBy, setSortBy] = useState('createdAt');
+    const [sortOrder, setSortOrder] = useState(1);
+
+    const handleOrderClick = (key) => {
+        if (key === sortBy) {
+            setSortOrder(prev => prev === 1 ? -1 : 1);
+        } else {
+            setSortBy(key);
+            setSortOrder(1);
+        }
+    }
+
+    useMemo(() => {
+        if (!tasks) return;
+
+        tasks.sort((a, b) => {
+            switch (sortBy) {
+                case 'createdAt':
+                    const dateA = new Date(a.createdAt).getTime();
+                    const dateB = new Date(b.createdAt).getTime();
+                    return (dateA - dateB) * sortOrder;
+
+                case 'title':
+                    return a.title.localeCompare(b.title) * sortOrder;
+
+                case 'status':
+                    const statusValue = {
+                        'Todo': 1,
+                        'Doing': 2,
+                        'Done': 3
+                    };
+                    const statusA = a.status.replace(" ", "");
+                    const statusB = b.status.replace(" ", "");
+                    return (statusValue[statusA] - statusValue[statusB]) * sortOrder;
+            }
+        })
+
+    }, [tasks, sortBy, sortOrder])
 
     if (!tasks) return <Loader />
+
 
     return (
         <div className="container mt-4">
@@ -15,9 +54,45 @@ const TaskList = memo(() => {
                 <table className="table table-dark table-striped table-hover">
                     <thead className="table-dark">
                         <tr>
-                            <th scope="col" className="fw-semibold">Nome</th>
-                            <th scope="col" className="fw-semibold">Stato</th>
-                            <th scope="col" className="fw-semibold">Data di Creazione</th>
+                            <th
+                                scope="col"
+                                className="fw-semibold cursor-pointer"
+                                onClick={() => handleOrderClick('title')}
+                                style={{width: '40%'}}
+                            >
+                                Nome
+                                {sortBy === 'title' &&
+                                    <span>
+                                        <i className={sortOrder === 1 ? "fa-solid fa-arrow-down-a-z ms-1" : "fa-solid fa-arrow-down-z-a ms-1"} />
+                                    </span>
+                                }
+                            </th>
+                            <th
+                                scope="col"
+                                className="fw-semibold cursor-pointer"
+                                onClick={() => handleOrderClick('status')}
+                                style={{width: '20%'}}
+                            >
+                                Stato
+                                {sortBy === 'status' &&
+                                    <span>
+                                        <i className={sortOrder === 1 ? "fa-solid fa-arrow-down-1-9 ms-1" : "fa-solid fa-arrow-down-9-1 ms-1"} />
+                                    </span>
+                                }
+                            </th>
+                            <th
+                                scope="col"
+                                className="fw-semibold cursor-pointer"
+                                onClick={() => handleOrderClick('createdAt')}
+                                style={{width: '40%'}}
+                            >
+                                Data di Creazione
+                                {sortBy === 'createdAt' &&
+                                    <span>
+                                        <i className={sortOrder === 1 ? "fa-solid fa-arrow-down-1-9 ms-1" : "fa-solid fa-arrow-down-9-1 ms-1"} />
+                                    </span>
+                                }
+                            </th>
                         </tr>
                     </thead>
                     <tbody>
